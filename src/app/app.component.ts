826db84eaab457acfc05eddddddeb22edbfe5d24
Alan Beck <beckalan303@gmail.com>
2020-12-23 13:49:36 -0300

    add new project contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { decrement, increment } from './contador/contador.actions';
+
+interface AppState{
+  contador: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'redux02';
+  contador: number;
+
+  constructor( private store: Store<AppState> ){
+    this.store.subscribe(state => {
+      console.log(state);
+      this.contador = state.contador;
+    })
+  }
+
+  incrementar(){
+    this.store.dispatch(increment());
+  }
+
+  decrementar(){
+    this.store.dispatch(decrement());
+  }
+  
+  cambioContador(event){
+  }
+
 }
---
+++ src/app/contador/contador.actions.ts
@@ -0,0 +1,4 @@
+import { createAction } from '@ngrx/store';
+
+export const increment = createAction('[contador] Increment');
+export const decrement = createAction('[contador] Decrement');
